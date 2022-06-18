4d552e95da81e86290cb1349ff9582b3f7742691
Kasir <kasir.barati@gmail.com>
2022-06-19 01:38:41 +0430

    add: task service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,17 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
+  providers: [TaskService],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe({
+      next: (tasks) => {
+        this.tasks = tasks;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.info('Tasks gotten');
+      },
+    });
+  }
 }
+
+/**
+ * Wrong usage:
+ * this.taskService.getTasks().subscribe(
+ *   (tasks) => {
+ *     this.tasks = tasks;
+ *   },
+ *   (error) => {
+ *     console.error(error);
+ *   },
+ *   () => {
+ *     console.info('Tasks gotten');
+ *   },
+ * );
+ */
+
+/**
+ * IDK why, but this is not necessary
+ * - providers: [TaskService]
+ */
