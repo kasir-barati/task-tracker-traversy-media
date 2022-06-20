5657859768992f77c1fef8e4fdfa7cf4488ef834
Kasir <kasir.barati@gmail.com>
2022-06-20 21:42:04 +0430

    update: use environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+import { Environment } from './environment.type';
+
+export const environment: Environment = {
   production: false,
+  apiUrl: 'http://localhost:5000/',
 };
 
 /*
