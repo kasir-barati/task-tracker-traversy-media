395327fdc9ade86f2ba1cd68345a135ee668b982
Kasir <kasir.barati@gmail.com>
2022-06-21 01:01:31 +0430

    fix: environment.ts wrong value

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import { Environment } from './environment.type';
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/',
+  apiUrl: 'http://localhost:5000',
 };
 
 /*
